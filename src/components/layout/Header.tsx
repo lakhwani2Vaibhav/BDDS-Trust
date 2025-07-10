f49c029cfe53cc2e875f28d902ae79379ef1cf0f
Vaibhav Lakhwani <vaibhavlakhwanivns@gmail.com>
2025-07-10 05:52:03 +0000

    in tablet view this is creating issue

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -63,8 +63,8 @@ export default function Header() {
                     </Button>
                 </SheetTrigger>
                 <SheetContent side="right">
-                    <SheetHeader className="sr-only">
-                        <SheetTitle>Mobile Menu</SheetTitle>
+                    <SheetHeader>
+                        <SheetTitle className="sr-only">Mobile Menu</SheetTitle>
                     </SheetHeader>
                     <div className="flex flex-col gap-6 pt-6">
                     <Link href="/" className="flex items-center gap-2 font-bold text-lg px-6" onClick={() => setMobileMenuOpen(false)}>
